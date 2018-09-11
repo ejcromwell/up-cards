67b2ee4761f9f732f61590113f82e08544e2ae1f
ejcromwell_github <elliot@eggbeater.ca>
2018-09-11 21:48:57 +0100

    Setup webpack

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Pulling in Bootstrap core CSS for brevity. Would use concatenated version in production -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/app.css">
 
     <title>Upshoot Playing Cards</title>
 </head>
